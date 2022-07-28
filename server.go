0ed1622b3ce8d0e84ce09db96001e89632f74e56
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2022-07-28 16:49:44 -0300

    Montando arquivo de configmap dentro do Container
    Signed-off-by: Nilson R S Vieira <nilsonrsvieira@gmail.com>

--- server.go
+++ server.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 import "net/http"
 import "os"
+import "io/ioutil"
+import "log"
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
 }
@@ -13,4 +16,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello i am  %s. I am %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/src/goapp/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file ", err)
+	}
+	fmt.Fprintf(w, "My Family: %s", string(data))
 }
