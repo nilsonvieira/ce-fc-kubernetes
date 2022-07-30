56fca00e09ade53400a391df52814f95499a5f9c
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2022-07-30 11:17:07 -0300

    Criando rota de health check e usando livenessProbe
    Signed-off-by: Nilson R S Vieira <nilsonrsvieira@gmail.com>

--- server.go
+++ server.go
@@ -5,9 +5,14 @@ import "net/http"
 import "os"
 import "io/ioutil"
 import "log"
+import "time"
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
 }
@@ -24,4 +29,22 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Error reading file ", err)
 	}
 	fmt.Fprintf(w, "My Family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "Hello my user is  %s. and my Password %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	}else{
+		w.WriteHeader(200)
+		w.Write([]byte("Status Up"))
+	}
+	
 }
